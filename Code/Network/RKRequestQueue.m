0460e7f07d2421e8d2d7c982d55d495904834d11
Blake Watters <blakewatters@gmail.com>
2012-06-18 23:57:42 -0400

    Do not emit didFinishLoading unless there is not a nextRequest

--- Code/Network/RKRequestQueue.m
+++ Code/Network/RKRequestQueue.m
@@ -218,7 +218,7 @@ static const NSTimeInterval kFlushDelay = 0.3;
 }
 
 - (void)removeLoadingRequest:(RKRequest*)request {
-    if (self.loadingCount == 1 && [_loadingRequests containsObject:request]) {
+    if ([self nextRequest] == nil && self.loadingCount == 1 && [_loadingRequests containsObject:request]) {
         RKLogTrace(@"Loading count decreasing from 1 to 0. Firing requestQueueDidFinishLoading");
 
         // Transition from processing to empty
@@ -250,8 +250,7 @@ static const NSTimeInterval kFlushDelay = 0.3;
 }
 
 - (RKRequest*)nextRequest {
-    for (NSUInteger i = 0; i < [_requests count]; i++) {
-        RKRequest* request = [_requests objectAtIndex:i];
+    for (RKRequest *request in _requests) {
         if ([request isUnsent]) {
             return request;
         }
